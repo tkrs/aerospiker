71c792e1818fc897d5c6bfac65db292344301e26
Takeru Sato <midium.size@gmail.com>
2015-06-12 19:45:09 +0900

    test improvement

--- src/test/scala/src/org/aerospiker/OperationSpec.scala
+++ src/test/scala/src/org/aerospiker/OperationSpec.scala
@@ -5,6 +5,7 @@ import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import org.scalatest._
+import org.scalatest.Assertions._
 
 class OperationSpec extends FlatSpec with Matchers {
 
@@ -44,7 +45,7 @@ class OperationSpec extends FlatSpec with Matchers {
       )
     ))
     val allData = new Bin("data", new Value(
-      List("string", true, null, 1e9, -(1L << 63), 0.1234568.toFloat, -(1 << 33), Array(0x02.toByte))
+      List("string", true, null, 1e9, (1L << 63) - 1, 0.1234568.toFloat, (1 << 33) - 1, Array(0x02.toByte))
     ))
 
     {
@@ -53,7 +54,7 @@ class OperationSpec extends FlatSpec with Matchers {
         case msg => println(s"string put done msg[$msg]")
       }
       f onFailure {
-        case e => println(s"errrrrrrror msg[$e]")
+        case e => println(s"$e"); fail()
       }
       Await.result(f, Duration(100, "millis"))
     }
@@ -62,6 +63,9 @@ class OperationSpec extends FlatSpec with Matchers {
       val f = client.get(key).run
       f onSuccess {
         case msg => println(s"get done msg[$msg]")
+      }
+      f onFailure {
+        case e => println(s"$e"); fail()
       }
       Await.result(f, Duration(100, "millis"))
     }
@@ -79,7 +83,10 @@ class OperationSpec extends FlatSpec with Matchers {
       val key = new Key("test", "teste", "not")
       val f = client.get(key).run
       f onSuccess {
-        case msg => println(s"string put error msg[$msg]")
+        case e => println(s"key not found msg [$e]")
+      }
+      f onFailure {
+        case e => println(s"key not found error [$e]")
       }
       Await.result(f, Duration(100, "millis"))
     }
@@ -88,7 +95,10 @@ class OperationSpec extends FlatSpec with Matchers {
       val key = new Key("test", "not", "not")
       val f = client.get(key).run
       f onSuccess {
-        case msg => println(s"string put error msg[$msg]")
+        case e => println(s"set not found msg [$e]")
+      }
+      f onFailure {
+        case e => println(s"set not found error [$e]")
       }
       Await.result(f, Duration(100, "millis"))
     }
@@ -97,7 +107,10 @@ class OperationSpec extends FlatSpec with Matchers {
       val key = new Key("not", "not", "not")
       val f = client.get(key).run
       f onSuccess {
-        case msg => println(s"string put error msg[$msg]")
+        case e => println(s"namespece not found msg [$e]")
+      }
+      f onFailure {
+        case e => println(s"namespece not found error [$e]")
       }
       Await.result(f, Duration(100, "millis"))
     }
@@ -111,6 +124,7 @@ class OperationSpec extends FlatSpec with Matchers {
       val errHosts = Array(Host("127.0.0.1", 9090))
       val settings = Settings(host = errHosts)
       val client = Client(settings)
+      fail()
     } catch {
       case e => println(e)
     }
