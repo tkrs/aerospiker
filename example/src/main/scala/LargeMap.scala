ec3df0bf3532c62fd8015427a67ead4debc1afdf
Takeru Sato <midium.size@gmail.com>
2015-10-07 09:18:51 +0900

    Change the return type of AerospikeLargeMap#get

--- example/src/main/scala/LargeMap.scala
+++ example/src/main/scala/LargeMap.scala
@@ -1,7 +1,6 @@
 import cats.data.Xor._
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
-import shapeless.HNil
 
 object LargeMap extends App with LazyLogging {
 
@@ -10,6 +9,8 @@ object LargeMap extends App with LazyLogging {
   import task.AerospikeLargeMap
 
   case class User(name: String, age: Int, now: Long, bbb: Seq[Double])
+  type Users = Map[String, User]
+
   val u1 = User("tkrs", 31, System.currentTimeMillis(), Seq(1.2, 3.4))
   val u2 = User("boo", 5, System.currentTimeMillis(), Seq(1.2, 3.4))
   val u3 = User("xyz", 99, System.currentTimeMillis(), Seq(1.00000002, 3.4))
@@ -24,14 +25,12 @@ object LargeMap extends App with LazyLogging {
     override protected def binName: String = "list"
   }
 
-  type Users = Map[String, User]
-
   lmap.put("u1", u1).run match {
     case Left(e) => e.printStackTrace()
     case Right(v) => logger.info(v.toString)
   }
 
-  lmap.get[Users]("u1").run match {
+  lmap.get[User]("u1").run match {
     case Left(e) => e.printStackTrace()
     case Right(v) => logger.info(v.toString)
   }
